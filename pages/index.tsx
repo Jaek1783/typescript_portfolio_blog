d05ceb3508164f4b0728ccda705f15a5f0e77f68
jaekeun Choi <jaek1783@gmail.com>
2023-04-25 22:29:57 +0900

    기본 페이지 구성

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+const Home = ()=>{
+    return(
+        <div>
+            <h1>Home Page</h1>
+            {/* <ul>
+                <li><Link href="https://github.com/jaek1783" target='_blank' >깃허브 바로가기</Link></li>
+                <li><Link href="https://blog.naver.com/wormsbrother" target='_blank' >블로그 바로가기</Link></li>
+            </ul> */}
+        </div>
+    )
+}
+
+export default Home;
