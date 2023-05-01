e8918bef4349774743c92b57546ffa1422d674da
jaekeun Choi <jaek1783@gmail.com>
2023-05-01 19:45:58 +0900

    배포를 위한 설정변경

--- server/dbConfig.js
+++ server/dbConfig.js
@@ -1,10 +1,5 @@
 require('dotenv').config();
 
-const fs = require('fs');
-
-
-const data = fs.readFileSync('./db/db.json');
-const conf = JSON.parse(data);
 const mysql = require('mysql');
 
 const db = mysql.createConnection({
