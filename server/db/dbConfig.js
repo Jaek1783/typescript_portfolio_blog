92e93f9f850d073ce6fd10885b62614cfb462e37
jaekeun Choi <jaek1783@gmail.com>
2023-05-01 19:25:03 +0900

    env파일 추가

--- server/db/dbConfig.js
+++ server/db/dbConfig.js
@@ -1,15 +1,18 @@
+require('dotenv').config();
+
 const fs = require('fs');
+
 
 const data = fs.readFileSync('./db/db.json');
 const conf = JSON.parse(data);
 const mysql = require('mysql');
 
 const db = mysql.createConnection({
-    host:conf.host,
-    user:conf.user,
-    password:conf.password,
-    port:conf.port,
-    database:conf.database
+    host:process.env.DB_HOST,
+    user:process.env.DB_USER,
+    password:process.env.DB_PW,
+    port:process.env.DB_PORT,
+    database:process.env.DB_NAME
 })
 
 db.connect();
