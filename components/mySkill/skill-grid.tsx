29a5a37aeb49db170d86df45080adda9429b6c4e
jaekeun Choi <jaek1783@gmail.com>
2023-05-03 08:52:40 +0900

    이미지 배포 오류로 수정중

--- components/mySkill/skill-grid.tsx
+++ components/mySkill/skill-grid.tsx
@@ -1,9 +1,9 @@
 import SkillItem from "./skill-item";
 import styles from './skills.module.css';
 const SkillGrid = (props : any)=>{
-const {skills} = props;
+const {skills, htmlSrc} = props;
     return <ul className={styles.box}>
-        {skills.map((skill:any) => <SkillItem key={skill.title} title={skill.title} image={skill.image}/>)}
+        {skills.map((skill:any) => <SkillItem key={skill.title} title={skill.title} image={skill.image} htmlSrc={htmlSrc}/>)}
     </ul>
 }
 
--- components/mySkill/skill-item.tsx
+++ components/mySkill/skill-item.tsx
@@ -2,13 +2,13 @@ import Image from "next/image";
 import styles from './skills.module.css';
 
 const SkillItem = (props:any)=>{
-const {title, image } = props;
+const {title, image, htmlSrc} = props;
 
 const imagePath = `/images/skills/${image}`;
     return <li className={styles.item}>
                 <div>
                 <p>hover please!</p>
-                  <Image src={imagePath} alt={title} width={80} height={80}/>
+                  <Image src={htmlSrc} alt={title} width={80} height={80}/>
                   <span>{title}</span>
                 </div>
     </li>
