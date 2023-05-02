bdebbd714425dcc98c5b7740c0cf0d3cff4274dc
jaekeun Choi <jaek1783@gmail.com>
2023-05-02 11:10:12 +0900

    배포를 위한 수정

--- components/mySkill/my-skill.tsx
+++ components/mySkill/my-skill.tsx
@@ -3,13 +3,13 @@ import styles from './skills.module.css';
 const MySkillList = (props : any)=>{
     const skills : {title : string, image : string}[ 
     ] = [
-        {title : 'html5', image :  './images/skills/HTML5.png'},
-        {title : 'CSS3', image :  './images/skills/CSS3.png'},
-        {title : 'Javascript', image :  './images/skills/JavaScript.png'},
-        {title : 'Typescript', image :  '/images/skills/Typescript.png'},
-        {title : 'React', image :  './images/skills/React.png'},
-        {title : 'NextJS', image :  './images/skills/Nextjs.png'},
-        {title : 'MySQL', image :  './images/skills/mySql.png'},
+        {title : 'html5', image :  '/images/skills/html5.png'},
+        {title : 'CSS3', image :  '/images/skills/css3.png'},
+        {title : 'Javascript', image :  '/images/skills/javascript.png'},
+        {title : 'Typescript', image :  '/images/skills/typescript.png'},
+        {title : 'React', image :  '/images/skills/react.png'},
+        {title : 'NextJS', image :  '/images/skills/nextjs.png'},
+        {title : 'MySQL', image :  '/images/skills/mysql.png'},
     ];
     return(
         <section className={styles.container}>
