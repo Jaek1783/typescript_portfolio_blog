4c5ccebe74808ad7cae3ec0129b0cd298ebf8393
jaekeun Choi <jaek1783@gmail.com>
2023-05-03 01:01:49 +0900

    배포를 위한 수정중

--- components/mySkill/skill-item.tsx
+++ components/mySkill/skill-item.tsx
@@ -4,11 +4,11 @@ import styles from './skills.module.css';
 const SkillItem = (props:any)=>{
 const {title, image } = props;
 
-// const imagePath = `/images/skills/${image}`;
+const imagePath = `/images/skills/${image}`;
     return <li className={styles.item}>
                 <div>
                 <p>hover please!</p>
-                  <Image src='/images/skills/mysql.png' alt={title} width={80} height={80}/>
+                  <Image src={imagePath} alt={title} width={80} height={80}/>
                   <span>{title}</span>
                 </div>
     </li>
