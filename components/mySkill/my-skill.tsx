525775052a6adea9b3953b089b5aa71325e5e1b2
jaekeun Choi <jaek1783@gmail.com>
2023-05-03 00:27:30 +0900

    배포 수정중

--- components/mySkill/my-skill.tsx
+++ components/mySkill/my-skill.tsx
@@ -2,13 +2,13 @@ import SkillGrid from "./skill-grid";
 import styles from './skills.module.css';
 import axios from "axios";
 import { useState ,useEffect} from "react";
+
 const MySkillList = (props : any)=>{
     const [skillData, setSkillData] = useState([]);
 useEffect(()=>{
     axios.get('./skills.json')
     .then((response:any)=>setSkillData(response.data));
 },[]);
-console.log(skillData);
 
 ;    return(
         <section className={styles.container}>
