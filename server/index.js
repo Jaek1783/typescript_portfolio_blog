ecf84c8af79ab12ba44e92edb0995306ab08c204
jaekeun Choi <jaek1783@gmail.com>
2023-05-03 10:19:47 +0900

    server 수정중

--- server/index.js
+++ server/index.js
@@ -24,23 +24,24 @@ app.use('/',(req,res)=>{
   const name = req.body.name;
   const desc = req.body.desc;
 
-  db.query(`insert into contact(user_email,user_name,user_desc) 
-  value (?,?,?)`,
-  [email, name, desc], 
-  (err, results)=>{
-      if(err) throw err;
-      res.send(results);
-  })
+  db.query('SELECT * FROM contact', function (error, results) {
+    if (error) throw error;
+    res.send(results);
+  });
 });
-// app.use('/api/contact',(req,res)=>{
-//     const email = req.body.email;
-//     const name = req.body.name;
-//     const desc = req.body.desc;
-//     console.log(email,name,desc);
-//     db.query('SELECT * FROM contact', function (error, results) {
-//         if (error) throw error;
-//         res.send(results);
-//       });
+app.use('/api/contact',(req,res)=>{
+    const email = req.body.email;
+    const name = req.body.name;
+    const desc = req.body.desc;
+    console.log(email,name,desc);
+    db.query(`insert into contact(user_email,user_name,user_desc) 
+    value (?,?,?)`,
+    [email, name, desc], 
+    (err, results)=>{
+        if(err) throw err;
+        res.send(results);
+    });
+
 //     db.query(`insert into contact(user_email,user_name,user_desc) 
 //     value (?,?,?)`,
 //     [email, name, desc], 
@@ -48,4 +49,4 @@ app.use('/',(req,res)=>{
 //         if(err) throw err;
 //         res.send(results);
 //     })
-// })
+});
