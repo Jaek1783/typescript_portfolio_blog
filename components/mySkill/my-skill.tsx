a40faff156fb03a4580f5e199dfa06b4f127a702
jaekeun Choi <jaek1783@gmail.com>
2023-05-03 09:18:36 +0900

    이미지 배포를 위한 수정중

--- components/mySkill/my-skill.tsx
+++ components/mySkill/my-skill.tsx
@@ -2,20 +2,17 @@ import SkillGrid from "./skill-grid";
 import styles from './skills.module.css';
 import axios from "axios";
 import { useState ,useEffect} from "react";
-import html from '../../public/images/skills/html5.png';
+
 const MySkillList = (props : any)=>{
     const [skillData, setSkillData] = useState([]);
-    const [htmlSrc, setHtmlSrc] = useState('');
 useEffect(()=>{
     axios.get('./skills.json')
     .then((response:any)=>setSkillData(response.data));
-    setHtmlSrc(html.src);
 },[]);
-console.log(htmlSrc);
 ;    return(
         <section className={styles.container}>
             <h2>My Skill List</h2>
-            <SkillGrid skills = {skillData} htmlSrc = {htmlSrc}/>
+            <SkillGrid skills = {skillData}/>
         </section>
     )
 }
